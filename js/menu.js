e97a713425c2f09274be370b5533fd4f9d06a455
DmitrievPlays <dmitriev8925@gmail.com>
2021-04-27 17:32:02 +0300

    Теперь свайпы работают только на телефонах)

--- js/menu.js
+++ js/menu.js
@@ -16,30 +16,50 @@ $(document).ready(function()
 
     var a = 0;
 
-    $(function() {
-    $(".game-type-carousel")
-        .on('mousedown touchstart', function (e) {
-            console.log("(x,y) = (" + e.pageX + "," + e.pageY +")");
-            xDown = e.pageX;
-            yDown = e.pageY;
-        })
-        .on('mouseup touchend',function (e) {
-            console.log("(x,y) = (" + e.pageX + "," + e.pageY +")");
-            xUp = e.pageX;
-            yUp = e.pageY;
-            if((xDown < xUp - 100) && (a < 0) && (a >= -1800))
-            {
-              a += 600;
-                $(".game-type-carousel-panel").css("left", a + "px");
-            }
-            else if((xDown > xUp + 100) && (a > -1800))
-            {
-              a -= 600;
-                $(".game-type-carousel-panel").css("left", a + "px");
-            }
-            //if ((xDown < Math.abs(xUp - 100) || xDown > Math.abs(xUp + 100)) || (yDown < Math.abs(yUp - 100) || yDown > Math.abs(yUp + 100))) {
 
-            //}
-        });
-    });
-})
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || evt.originalEvent.touches;
+}
+
+    function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 && (a > -1800) && (a <= 0)) {
+          a -= 600;
+            $(".game-type-carousel-panel").css("left", a + "px");
+        } else if ( xDiff < 0 && (a >= -1800) && (a < 0)){
+          a += 600;
+            $(".game-type-carousel-panel").css("left", a + "px");
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */
+        } else {
+            /* down swipe */
+        }
+    }
+    xDown = null;
+    yDown = null;
+};
+});
